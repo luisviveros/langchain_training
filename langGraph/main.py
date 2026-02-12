399ef67ca98840bc313264c83ba5cdf138ed07d1
Luis David Viveros Escamilla <luisviveros9810@outlook.com>
2026-02-12 12:25:50 -0600

    modify main.py

--- langGraph/main.py
+++ langGraph/main.py
@@ -1,11 +1,18 @@
 import os
+from typing import Literal
 from dotenv import load_dotenv
 from langchain_google_genai import ChatGoogleGenerativeAI
 from pydantic import BaseModel
 from langgraph.graph import StateGraph, START, END
+from langchain_core.prompts import PromptTemplate
 
 load_dotenv()
-llm = ChatGoogleGenerativeAI(model="gemini-2.0-flash", temperature=0)
+
+class GreetingOutput(BaseModel):
+    greeting: bool
+
+class LanguageOutput(BaseModel):
+    language: Literal["es", "en"]
 
 class AgentState(BaseModel):
     """
@@ -21,30 +28,70 @@ initial_state.user_message += "!"
 print(initial_state.user_message)
 
 def greeting_node(agent_state: AgentState)-> AgentState:
-    if "Hola" in agent_state.user_message:
-        agent_state.greeting = True
-    else:
-        agent_state.greeting = False
+    prompt_template = PromptTemplate.from_template(
+    """
+    Debes detectar si en el siguiente texto el usuario esta saludando: {text}
+    Tu respuesta debera ser true si detectas que el usuario esta saludando o 
+    false si no esta saludando.
+    """
+    )
+    llm = ChatGoogleGenerativeAI(model="gemini-2.0-flash", temperature=0)
+    llm_parsed = llm.with_structured_output(GreetingOutput)
+    chain = prompt_template | llm_parsed
+    chain_response: GreetingOutput = chain.invoke({"text": agent_state.user_message})
+    agent_state.greeting = chain_response.greeting
     return agent_state
 
 def response_node(agent_state: AgentState) -> AgentState:
     if agent_state.greeting:
         agent_state.agent_response = "¡Hola! ¿En qué puedo ayudarte hoy?"
     else:
-        agent_state.agent_response = "Vuelve a intentarlo"
+         llm = ChatGoogleGenerativeAI(model="gemini-2.0-flash", temperature=0)
+         llm_response = llm.invoke(agent_state.user_message)
+         agent_state.agent_response = llm_response.content
     return agent_state
 
+def evaluate_response(agent_state: AgentState) -> Literal["spanish_response_node", "english_response_node"]:
+
+    llm = ChatGoogleGenerativeAI(model="gemini-2.0-flash", temperature=0)
+    llm_parsed = llm.with_structured_output(LanguageOutput)
+    llm_respone : LanguageOutput = llm_parsed.invoke(
+        f"""
+        En que lenguaje esta el siguiente texto, devuelve un json con el lenguaje 'es' en el caso de que 
+        texto este en espanol o 'en' en el caso de que este en ingles
+        Texto: {agent_state.agent_response}
+        """
+    )
+    if llm_respone.language == "es":
+        return "spanish_response_node"
+    else:
+        return "english_response_node"
+
+
+def spanish_response_node(agent_state: AgentState) -> AgentState:
+    agent_state.agent_response += "\nIdioma: Espanol"
+    return agent_state
+
+def english_response_node(agent_state: AgentState) -> AgentState:
+    agent_state.agent_response += "\nIdioma: Ingles"
+    return agent_state
 
 graph = StateGraph(AgentState)
 graph.add_node(greeting_node)
 graph.add_node(response_node)
+graph.add_node(spanish_response_node)
+graph.add_node(english_response_node)
 
 graph.add_edge(START, "greeting_node")
 graph.add_edge("greeting_node", "response_node")
-graph.add_edge("response_node", END)
+#graph.add_edge("response_node", END)
+graph.add_conditional_edges("response_node", evaluate_response)
+graph.add_edge("spanish_response_node", END)
+graph.add_edge("english_response_node", END)
 
 compiled_graph = graph.compile()
 
-initial_state = AgentState(user_message="Hola")
-respone = compiled_graph.invoke(initial_state)
-print(respone["agent_response"])
+if __name__ == "__main__":
+    initial_state = AgentState(user_message="Hola")
+    response = compiled_graph.invoke(initial_state)
+    print(response["agent_response"])
